d4f839065306afcb1bdbd49af2fe2a40d6dc7880
Ahmed Al-Zubidy <aalzubidy90@gmail.com>
2022-03-10 13:54:58 -0800

    Removed url tags (#4)
    * Minor/react snap (#1)
    
    * Added react snap and react helmet
    
    * Added react snap and react helmet
    
    * Updated url tag

--- src/Components/JSONBeautify/index.js
+++ src/Components/JSONBeautify/index.js
@@ -1,7 +1,6 @@
 import { useEffect, useState } from 'react';
 import { Tooltip } from '@mui/material';
 import { Helmet } from 'react-helmet';
-import Paths from '../../AppRouter/Paths';
 import { copyTextToClipBoard, downloadFile } from '../../Helpers';
 import CustomAceEditor from '../CustomAceEditor';
 import './jsonBeautify.scss';
@@ -48,8 +47,6 @@ const JSONBeautify = () => {
         <title>JSON Tools Plus - JSON Beautify</title>
         <meta name="pagename" content="JSON Tools Plus - JSON Beautify" />
         <meta name="subject" content="JSON Tools Plus - JSON Beautify" />
-        <meta name="url" content={`http://www.jsontoolsplus.com${Paths.jsonBeautify}`} />
-        <meta name="identifier-URL" content={`http://www.jsontoolsplus.com${Paths.jsonBeautify}`} />
       </Helmet>
 
       <div className='row pathInput'>
